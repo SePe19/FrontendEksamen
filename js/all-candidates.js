a378459c4509f71f0b5c3d3721742feefc1418cc
Sepe19 <seba217i@stud.kea.dk>
2021-12-20 23:46:39 +0100

    allcandidates

--- js/all-candidates.js
+++ js/all-candidates.js
@@ -15,19 +15,110 @@ function candidateData(data) {
 
     const candidate = data[i];
 
-
     let candidateDiv = document.createElement('div')
 
     mainDiv.append(candidateDiv)
 
     let candidateName = document.createElement('span')
-    candidateName.setAttribute('value', candidate.candidateName)
-    candidateName.innerHTML = candidate.candidateName
+    candidateName.setAttribute('value', candidate.candidateName);
+    candidateName.innerText = candidate.candidateName;
 
     candidateDiv.append(candidateName)
     out(candidateName)
 
+    let candidateInput = document.createElement('input')
+    candidateInput.type = 'text'
+    candidateInput.setAttribute('value', candidateName.innerText);
 
+    candidateName.addEventListener('click', event => {
+      candidateInput.value = candidate.candidateName;
+      candidateName.replaceWith(candidateInput)
+    })
+
+    const updateButton = document.createElement('button')
+    updateButton.classList.add('profile-edit-btn', 'btn', 'btn-primary', 'updateButtonProject');
+    updateButton.type = 'button'
+    updateButton.innerText = 'Save'
+    candidateDiv.append(updateButton)
+
+    updateButton.onclick = function () {
+      candidate.candidateName = candidateInput.value;
+      updateCandidate(candidate)
+    }
+
+    const deleteButton = document.createElement('button')
+    deleteButton.classList.add('profile-edit-btn', 'btn', 'btn-danger', 'deleteButton');
+    deleteButton.type = 'button'
+    deleteButton.innerText = 'Delete'
+    candidateDiv.append(deleteButton)
+
+    deleteButton.onclick = function () {
+      deleteCandidate(candidate)
+    }
   }
 }
+
+async function updateCandidate(candidate) {
+  try {
+    const response = await restUpdateCandidate(candidate);
+    out(response);
+    window.location.reload();
+  } catch (error) {
+    out(error);
+  }
+
+}
+
+async function restUpdateCandidate(candidate) {
+  const updateURL = "http://localhost:8080/candidate/" + candidate.candidateId;
+  const jsonString = JSON.stringify(candidate);
+  out(jsonString);
+
+  const fetchOptions = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: jsonString
+  }
+  const response = await fetch(updateURL, fetchOptions);
+  out(response.status);
+  out(response.ok);
+  if (!response.ok) {
+    out("error");
+  }
+  return response.json();
+}
+
+async function deleteCandidate(candidate) {
+  try {
+    const response = await restDeleteProject(candidate);
+    out(response);
+    window.location.reload();
+  } catch (error) {
+    out(error);
+  }
+}
+
+async function restDeleteProject(candidate) {
+  const deleteUrl = "http://localhost:8080/candidate/" + candidate.candidateId;
+  const fetchOptions = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: ""
+  }
+
+  const response = await fetch(deleteUrl, fetchOptions);
+  out(response.status)
+  out(response.ok)
+  window.location.href = "http://localhost:63342/FrontendEksamenKommunalvalg/all-candidates.html?_ijt=aulfoe929rq3euj7q4b9d6inkm&_ij_reload=RELOAD_ON_SAVE"
+  if (!response.ok) {
+    out(response.error())
+  }
+
+  return response;
+}
+
 fetchCandidates();
--- js/create-candidate.js
+++ js/create-candidate.js
@@ -1,90 +1,78 @@
-const out = (str) => console.log(str);
-let currentUserId = localStorage.getItem('currentUserId');
-const customerUrl = "http://localhost:8080/userCustomers/" + currentUserId;
+const out = (...str) => console.log(...str);
+const partyUrl = "http://localhost:8080/showAllParties";
+const candidateUrl = "http://localhost:8080/candidate";
 
-function newProject() {
-  let title = document.getElementById("inpTitle").value
-  let description = document.getElementById("inpDescription").value
+function newCandidate() {
+  let candidateName = document.getElementById("inpName").value
 
-  let chosenCustomer = localStorage.getItem('chosenCustomer');
-  out(chosenCustomer)
+  let chosenParty = localStorage.getItem('chosenParty');
+  out(chosenParty)
 
-  let postProjectRequest = {
+  let postCandidateRequest = {
     method: "POST",
     headers: {
       "content-type": "application/json"
     },
     body: JSON.stringify({
-      "projectTitle": title,
-      "projectDescription": description,
-      "user": {
-        "userID": currentUserId,
-      },
-      "customer":
+      "candidateName": candidateName,
+      "party":
         {
-          "customerId": chosenCustomer,
+          "partyId": chosenParty,
         }
     })
   }
 
-  out(postProjectRequest)
+  out(postCandidateRequest)
 
 
-  fetch("http://localhost:8080/project", postProjectRequest)
+  fetch(candidateUrl, postCandidateRequest)
     .then(response => response.json())
-    .then(data => projectCreated(data))
+    .then(data => candidateCreated(data))
     .catch(error => console.log(error));
 }
 
-function projectCreated(data){
+function candidateCreated(data){
   out(arguments)
   out(data)
-  window.location.href = "/Tidsregistrering-projekt-Frontend/project.html";
+  //window.location.href = "/Tidsregistrering-projekt-Frontend/project.html";
 }
 
-function fetchAllCustomersOnUser() {
-  return fetch(customerUrl)
+function fetchAllParties() {
+  return fetch(partyUrl)
     .then(data => data.json())
-    .then(customerDropdown);
-
+    .then(partyDropdown);
 }
 
-function customerDropdown(data) {
-  let firstCustomer = data[0];
-  let firstCustomerId = firstCustomer[Object.keys(firstCustomer)[0]];
-  localStorage.setItem('chosenCustomer', firstCustomerId)
+function partyDropdown(data) {
+  let firstParty= data[0];
+  let firstPartyId = firstParty[Object.keys(firstParty)[0]];
+  localStorage.setItem('chosenParty', firstPartyId)
 
-  out(firstCustomerId + " Her outter vi vores data")
+  out(firstPartyId + " Her outter vi vores data")
 
   for (let i = 0; i < data.length; i++) {
 
-    let customer = data[i];
+    let party = data[i];
 
     let dropdown = document.getElementById("selectDropdown");
     let option = document.createElement("option");
-    option.innerText = customer.customerName;
+    option.innerText = party.partyName;
     option.setAttribute("class", "select-dropdown__list-item");
-    option.setAttribute("value", customer.customerId);
+    option.setAttribute("value", party.partyId);
     dropdown.appendChild(option);
 
     //out(customer[0].customerId + " Her outter vi customer [0]")
 
-
-
     dropdown.addEventListener("change",(event) => {
       const selectIndex = dropdown.selectedIndex;
       let optionIndex = dropdown.options[selectIndex]
-      customer.customerId = optionIndex.value
+      party.partyId = optionIndex.value
       out(optionIndex.value);
-      localStorage.setItem("chosenCustomer", optionIndex.value)
+      localStorage.setItem("chosenParty", optionIndex.value)
 
     })
   }
 
 }
 
-
-
-
-
-fetchAllCustomersOnUser();
+fetchAllParties();
